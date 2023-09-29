7fcebfcdd15e42cb22a4f251b49941fca7df4122
Jared Mathews <boat@sea.local>
2023-09-28 23:09:29 -0600

    release(0.1.0): port from nodejs based stew.io repo to new deno based tooling

---
+++ source/music.module.ts
@@ -0,0 +1,55 @@
+import { SegmentModule } from "stew/config";
+import { MusicItem } from "./MusicItem.ts";
+import { MusicItemDisplay } from "./MusicItemDisplay.tsx";
+
+export default {
+  SegmentItemDisplay: MusicItemDisplay,
+  segmentSortOptions: [
+    {
+      sortOptionKey: "musicTitleAsc",
+      sortOptionLabel: "title: a → z",
+      getSortOrder: (musicItemA, musicItemB) =>
+        musicItemA.musicTitle.localeCompare(musicItemB.musicTitle),
+    },
+    {
+      sortOptionKey: "musicTitleDesc",
+      sortOptionLabel: "title: z → a",
+      getSortOrder: (musicItemA, musicItemB) =>
+        musicItemB.musicTitle.localeCompare(musicItemA.musicTitle),
+    },
+    {
+      sortOptionKey: "musicArtistAsc",
+      sortOptionLabel: "artist: a → z",
+      getSortOrder: (musicItemA, musicItemB) =>
+        musicItemA.musicArtist[0].localeCompare(musicItemB.musicArtist[0]),
+    },
+    {
+      sortOptionKey: "musicArtistDesc",
+      sortOptionLabel: "artist: z → a",
+      getSortOrder: (musicItemA, musicItemB) =>
+        musicItemB.musicArtist[0].localeCompare(musicItemA.musicArtist[0]),
+    },
+    {
+      sortOptionKey: "musicYearAsc",
+      sortOptionLabel: "year: 0 → 9",
+      getSortOrder: (musicItemA, musicItemB) =>
+        musicItemA.musicYear - musicItemB.musicYear,
+    },
+    {
+      sortOptionKey: "musicYearDesc",
+      sortOptionLabel: "year: 9 → 0",
+      getSortOrder: (musicItemA, musicItemB) =>
+        musicItemB.musicYear - musicItemA.musicYear,
+    },
+  ],
+  getSegmentItemSearchString: (someMusicItem: MusicItem) =>
+    `${someMusicItem.musicTitle},${someMusicItem.musicArtist.join(
+      ","
+    )},${someMusicItem.musicTags.join(",")},${
+      someMusicItem.musicYear
+    },${`${someMusicItem.recordingContext.join("/")} ${
+      someMusicItem.sourceType === "collection"
+        ? someMusicItem.collectionType
+        : someMusicItem.sourceType
+    }${someMusicItem.musicType === "clip" ? " clip" : ""}`}`,
+} satisfies SegmentModule<MusicItem>;
---
+++ source/stew.config.ts
@@ -0,0 +1,322 @@
+import { SourceStewConfig } from "stew/config";
+import {
+  getEmailIconSvg,
+  getInstagramIconSvg,
+  getTwitterIconSvg,
+  getWebsiteIconSvg,
+} from "stew/config/helpers";
+import { musicDataset } from "./music.dataset.ts";
+
+export default getStewConfig();
+
+function getStewConfig(): SourceStewConfig {
+  const plDataset = musicDataset.filter(
+    (someMusicItem) =>
+      someMusicItem.musicArtist.findIndex(
+        (someMusicArtist) => someMusicArtist === "Pretty Lights"
+      ) >= 0
+  );
+  const menertDataset = musicDataset.filter(
+    (someMusicItem) =>
+      someMusicItem.musicArtist.findIndex(
+        (someMusicArtist) => someMusicArtist === "Michal Menert"
+      ) >= 0
+  );
+  const relatedDataset = musicDataset.filter(
+    (someMusicItem) =>
+      someMusicItem.musicArtist.findIndex(
+        (someMusicArtist) =>
+          someMusicArtist === "Break Science" ||
+          someMusicArtist === "Paul Basic" ||
+          someMusicArtist === "Eliot Lipp" ||
+          someMusicArtist === "Paper Diamond" ||
+          someMusicArtist === "Motifv" ||
+          someMusicArtist === "Gramatik" ||
+          someMusicArtist === "SuperVision" ||
+          someMusicArtist === "Borahm Lee" ||
+          someMusicArtist === "Chris Karns"
+      ) >= 0
+  );
+  return {
+    stewInfo: {
+      stewName: "pl-archive",
+      stewTagline: "worldwide official",
+      stewMessage: "the time has come for bad things to end...",
+      stewExternalLinks: [
+        {
+          linkLabel: "website",
+          linkHref: "http://prettylightsmusic.com",
+          linkIconSvg: getWebsiteIconSvg(),
+        },
+        {
+          linkLabel: "twitter",
+          linkHref: "https://twitter.com/prettylights",
+          linkIconSvg: getTwitterIconSvg(),
+        },
+        {
+          linkLabel: "instagram",
+          linkHref: "https://www.instagram.com/prettylights/",
+          linkIconSvg: getInstagramIconSvg(),
+        },
+        {
+          linkLabel: "email",
+          linkHref: "mailto:pl-archive@stews.io",
+          linkIconSvg: getEmailIconSvg(),
+        },
+      ],
+    },
+    stewSegments: [
+      {
+        segmentDataset: plDataset,
+        segmentModulePath: "./music.module.ts",
+        segmentKey: "pl",
+        segmentLabel: "pl",
+        segmentViews: [
+          {
+            viewKey: "all",
+            viewLabel: "all",
+            viewItemIds: plDataset.map((somePlItem) => somePlItem.itemId),
+          },
+          {
+            viewKey: "soundship",
+            viewLabel: "soundship",
+            viewItemIds: plDataset.reduce<Array<number>>(
+              (viewItemsIdsResult, somePlItem) => {
+                if (
+                  somePlItem.musicTags.findIndex(
+                    (someMusicTag) => someMusicTag === "soundship spacesystem"
+                  ) >= 0
+                ) {
+                  viewItemsIdsResult.push(somePlItem.itemId);
+                }
+                return viewItemsIdsResult;
+              },
+              []
+            ),
+          },
+          {
+            viewKey: "discography",
+            viewLabel: "discography",
+            viewItemIds: plDataset.reduce<Array<number>>(
+              (viewItemsIdsResult, somePlItem) => {
+                if (
+                  somePlItem.musicTags.findIndex(
+                    (someMusicTag) => someMusicTag === "discography"
+                  ) >= 0
+                ) {
+                  viewItemsIdsResult.push(somePlItem.itemId);
+                }
+                return viewItemsIdsResult;
+              },
+              []
+            ),
+          },
+          {
+            viewKey: "unreleased",
+            viewLabel: "unreleased",
+            viewItemIds: plDataset.reduce<Array<number>>(
+              (viewItemsIdsResult, somePlItem) => {
+                if (
+                  somePlItem.musicTags.findIndex(
+                    (someMusicTag) => someMusicTag === "unreleased"
+                  ) >= 0
+                ) {
+                  viewItemsIdsResult.push(somePlItem.itemId);
+                }
+                return viewItemsIdsResult;
+              },
+              []
+            ),
+          },
+          {
+            viewKey: "shows",
+            viewLabel: "shows",
+            viewItemIds: plDataset.reduce<Array<number>>(
+              (viewItemsIdsResult, somePlItem) => {
+                if (
+                  somePlItem.sourceType === "mix" &&
+                  somePlItem.recordingContext.findIndex(
+                    (someRecordingContextItem) =>
+                      someRecordingContextItem === "concert"
+                  ) >= 0
+                ) {
+                  viewItemsIdsResult.push(somePlItem.itemId);
+                }
+                return viewItemsIdsResult;
+              },
+              []
+            ),
+          },
+          {
+            viewKey: "oh-gee",
+            viewLabel: "oh gee",
+            viewItemIds: plDataset.reduce<Array<number>>(
+              (viewItemsIdsResult, somePlItem) => {
+                if (
+                  somePlItem.musicTags.findIndex(
+                    (someMusicTag) => someMusicTag === "o.g."
+                  ) >= 0
+                ) {
+                  viewItemsIdsResult.push(somePlItem.itemId);
+                }
+                return viewItemsIdsResult;
+              },
+              []
+            ),
+          },
+        ],
+      },
+      {
+        segmentDataset: menertDataset,
+        segmentModulePath: "./music.module.ts",
+        segmentKey: "menert",
+        segmentLabel: "menert",
+        segmentViews: [
+          {
+            viewKey: "all",
+            viewLabel: "all",
+            viewItemIds: menertDataset.map(
+              (someMusicItem) => someMusicItem.itemId
+            ),
+          },
+          {
+            viewKey: "discography",
+            viewLabel: "discography",
+            viewItemIds: menertDataset.reduce<Array<number>>(
+              (viewItemsIdsResult, someMenertItem) => {
+                if (
+                  someMenertItem.musicTags.findIndex(
+                    (someMusicTag) => someMusicTag === "discography"
+                  ) >= 0
+                ) {
+                  viewItemsIdsResult.push(someMenertItem.itemId);
+                }
+                return viewItemsIdsResult;
+              },
+              []
+            ),
+          },
+        ],
+      },
+      {
+        segmentDataset: relatedDataset,
+        segmentModulePath: "./music.module.ts",
+        segmentKey: "related",
+        segmentLabel: "related",
+        segmentViews: [
+          {
+            viewKey: "all",
+            viewLabel: "all",
+            viewItemIds: relatedDataset.map(
+              (someMusicItem) => someMusicItem.itemId
+            ),
+          },
+          {
+            viewKey: "breakscience",
+            viewLabel: "break science",
+            viewItemIds: relatedDataset.reduce<Array<number>>(
+              (viewItemsIdsResult, someRelatedItem) => {
+                if (
+                  someRelatedItem.musicArtist.findIndex(
+                    (someArtistItem) =>
+                      someArtistItem === "Break Science" ||
+                      someArtistItem === "Borahm Lee"
+                  ) >= 0
+                ) {
+                  viewItemsIdsResult.push(someRelatedItem.itemId);
+                }
+                return viewItemsIdsResult;
+              },
+              []
+            ),
+          },
+          {
+            viewKey: "karns",
+            viewLabel: "karns",
+            viewItemIds: relatedDataset.reduce<Array<number>>(
+              (viewItemsIdsResult, someRelatedItem) => {
+                if (
+                  someRelatedItem.musicArtist.findIndex(
+                    (someArtistItem) => someArtistItem === "Chris Karns"
+                  ) >= 0
+                ) {
+                  viewItemsIdsResult.push(someRelatedItem.itemId);
+                }
+                return viewItemsIdsResult;
+              },
+              []
+            ),
+          },
+          {
+            viewKey: "motifv",
+            viewLabel: "motifv",
+            viewItemIds: relatedDataset.reduce<Array<number>>(
+              (viewItemsIdsResult, someRelatedItem) => {
+                if (
+                  someRelatedItem.musicArtist.findIndex(
+                    (someArtistItem) => someArtistItem === "Motifv"
+                  ) >= 0
+                ) {
+                  viewItemsIdsResult.push(someRelatedItem.itemId);
+                }
+                return viewItemsIdsResult;
+              },
+              []
+            ),
+          },
+          {
+            viewKey: "paulbasic",
+            viewLabel: "paul basic",
+            viewItemIds: relatedDataset.reduce<Array<number>>(
+              (viewItemsIdsResult, someRelatedItem) => {
+                if (
+                  someRelatedItem.musicArtist.findIndex(
+                    (someArtistItem) => someArtistItem === "Paul Basic"
+                  ) >= 0
+                ) {
+                  viewItemsIdsResult.push(someRelatedItem.itemId);
+                }
+                return viewItemsIdsResult;
+              },
+              []
+            ),
+          },
+          {
+            viewKey: "eliotlipp",
+            viewLabel: "eliot lipp",
+            viewItemIds: relatedDataset.reduce<Array<number>>(
+              (viewItemsIdsResult, someRelatedItem) => {
+                if (
+                  someRelatedItem.musicArtist.findIndex(
+                    (someArtistItem) => someArtistItem === "Eliot Lipp"
+                  ) >= 0
+                ) {
+                  viewItemsIdsResult.push(someRelatedItem.itemId);
+                }
+                return viewItemsIdsResult;
+              },
+              []
+            ),
+          },
+          {
+            viewKey: "supervision",
+            viewLabel: "supervision",
+            viewItemIds: relatedDataset.reduce<Array<number>>(
+              (viewItemsIdsResult, someRelatedItem) => {
+                if (
+                  someRelatedItem.musicArtist.findIndex(
+                    (someArtistItem) => someArtistItem === "SuperVision"
+                  ) >= 0
+                ) {
+                  viewItemsIdsResult.push(someRelatedItem.itemId);
+                }
+                return viewItemsIdsResult;
+              },
+              []
+            ),
+          },
+        ],
+      },
+    ],
+  };
+}
